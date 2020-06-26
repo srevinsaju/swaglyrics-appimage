a6a6b87a8c81adb692f2dffb4e3ebef2bec0f307
Srevin Saju <srevin03@gmail.com>
2020-06-26 16:28:07 +0300

    Initial commit :tada:

---
+++ appimage/entrypoint.sh
@@ -0,0 +1,4 @@
+export LD_LIBRARY_PATH="${APPDIR}/usr/lib:${LD_LIBRARY_PATH}"
+export PATH="${APPDIR}/usr/bin:${PATH}"
+
+{{ python-executable }} -s ${APPDIR}/opt/python{{ python-version }}/bin/swagylrics "$@"
