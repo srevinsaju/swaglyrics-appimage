c9d67c6cb430d3be53fb2beca327e1693e8fd085
Srevin Saju <srevin03@gmail.com>
2020-06-27 16:20:39 +0300

    Use dev-ss

--- appimage/entrypoint.sh
+++ appimage/entrypoint.sh
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH="${APPDIR}/usr/lib:${LD_LIBRARY_PATH}"
 export PATH="${APPDIR}/usr/bin:${PATH}"
 
-{{ python-executable }} -s ${APPDIR}/opt/python{{ python-version }}/bin/swagylrics "$@"
+{{ python-executable }} -s ${APPDIR}/usr/bin/swagylrics "$@"
